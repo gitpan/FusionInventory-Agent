d0476f888ee3f07a3329a311970f398a5b624690
FusionInventory Project <cpan@fusioninventory.org>
2011-12-14 12:12:45 +0000

    Import of FUSINV/FusionInventory-Agent-2.1.13 from CPAN.
    gitpan-cpan-distribution: FusionInventory-Agent
    gitpan-cpan-version:      2.1.13
    gitpan-cpan-path:         FUSINV/FusionInventory-Agent-2.1.13.tar.gz
    gitpan-cpan-author:       FUSINV
    gitpan-cpan-maturity:     released

--- lib/FusionInventory/Agent/Network.pm
+++ lib/FusionInventory/Agent/Network.pm
@@ -107,8 +107,11 @@ sub createUA {
 
     my $ua = LWP::UserAgent->new(keep_alive => 1, requests_redirectable => ['POST', 'GET', 'HEAD']);
 
-
-    if ($LWP::VERSION >= 6) {
+    # previously this block was only for LWP6.
+    # For some modern LWP5/Crypt::SSLeay also need it, I use
+    # an eval here to avoid failure on ancient LWP::UserAgent with
+    # no ssl_opts()
+    eval {
         # LWP6 default behavior is to check the SSL hostname
         if ($config->{'no-ssl-check'}) {
             $ua->ssl_opts(verify_hostname => 0);
@@ -119,7 +122,7 @@ sub createUA {
         if ($config->{'ca-cert-dir'}) {
             $ua->ssl_opts(SSL_ca_path => $config->{'ca-cert-dir'});
         }
-    }
+    };
 
     if ($noProxy) {
 
@@ -359,9 +362,10 @@ sub setSslRemoteHost {
     # Check server name against provided SSL certificate
         if ( $self->{URI} =~ /^https:\/\/([^\/]+).*$/i ) {
             my $re = $1;
+            $re =~ s/:\d+//;
 # Accept SSL cert will hostname with wild-card
 # http://forge.fusioninventory.org/issues/542
-            $re =~ s/^([^\.]+)/($1|\\*)/;
+            $re =~ s/^([^\.]+)\.(.+)/($1|\\*)/;
 # protect some characters, $re will be evaluated as a regex
             $re =~ s/([\-\.])/\\$1/g;
             $ua->default_header('If-SSL-Cert-Subject' => '/CN='.$re.'($|\/)');
